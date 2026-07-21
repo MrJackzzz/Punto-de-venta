7082ea3fe473898dd95d8c4c430caee667e28924
ALEJANDRO <91EZEQUIEL.F@GMAIL.COM>
2026-07-21 09:30:52 -0300

    Cleanup temp files

--- check_db.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker exec -i nexoracore-db-1 psql -U nexora -d nexoracore <<'SQL'
-SELECT key, value FROM config WHERE key IN ('logo_filename','favicon_data','logo_filename_b64_backup','favicon_data_b64_backup');
-SQL
