b9a7029d3669f7c5c0b577c04bb45705682ebbcb
ALEJANDRO <91EZEQUIEL.F@GMAIL.COM>
2026-07-23 08:34:26 -0300

    Cleanup temp files

--- find_c1.sh
+++
@@ -1,3 +0,0 @@
-docker inspect cliente1-web-1 --format '{{index .Config.Labels "com.docker.compose.project.working_dir"}}'
-docker inspect cliente1-web-1 --format '{{index .Config.Labels "com.docker.compose.project.config_files"}}'
-docker inspect cliente1-db-1 --format '{{index .Config.Labels "com.docker.compose.project.working_dir"}}'
